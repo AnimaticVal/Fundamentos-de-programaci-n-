5575d8fa50cdebc42b56b6f8a4e5bd8e872bfa8a
AnimaticVal <60115544+AnimaticVal@users.noreply.github.com>
2020-01-30 19:03:49 -0500

    Add files via upload

---
+++ EPS.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("ingrese la cantidad de salarios mínimos: ");
+            double smm = double.Parse(Console.ReadLine());
+            ;
+
+            //operaciones 
+
+            if (smm <= 2)
+            {
+
+                Console.WriteLine("Su tarifa es la A");
+
+            }
+            else if (2 < smm && smm < 5)
+            {
+
+                Console.WriteLine("Su tarifa es la B");
+            }
+            else
+            {
+
+                Console.WriteLine("Su tarifa es la C");
+
+            }
+        }
+    }
+}
---
+++ Tarifa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("ingrese la cantidad de salarios mínimos: ");
+            double smm = double.Parse(Console.ReadLine());
+        ;
+
+            //operaciones 
+
+            if (smm <= 2)
+            {
+
+                Console.WriteLine("Su tarifa es la A");
+
+            }
+            else if (2 < smm && smm < 4)
+            {
+
+                Console.WriteLine("Su tarifa es la B");
+            }
+            else {
+
+                Console.WriteLine("Su tarifa es la C");
+
+            }
+
+
+
+
+
+        }
+    }
+}
