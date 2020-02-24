81fdc80f9ab1948147eb0f81e0a4d611a9ac48db
AnimaticVal <60115544+AnimaticVal@users.noreply.github.com>
2020-02-24 15:15:58 -0500

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiclosFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el valor de x");
+            double x = double.Parse(Console.ReadLine());
+            double sen = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                double numerador = Math.Pow(-1, i);
+                double denominador = Factorial(2*i + 1);
+                sen += (numerador / denominador) * Math.Pow(x,2*i+1 );
+
+            }
+            Console.WriteLine(sen);
+
+        }
+        public static double Factorial(double number) {
+            if (number == 1) return 1;
+            else return number * Factorial(number - 1);
+        }
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiclosFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el valor de x");
+            double x = double.Parse(Console.ReadLine());
+            double e = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                double numerador = Math.Pow(x, i);
+                double denominador = Factorial(i);
+                e += (numerador / denominador) ;
+
+            }
+            Console.WriteLine(e);
+
+        }
+        public static double Factorial(double number) {
+            if (number == 0) return 1;
+            if (number == 1) return 1;
+            else return number * Factorial(number - 1);
+        }
+    }
+}
